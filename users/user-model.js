d1c0d17821e0eb8d5ffcbee87d75d350ad78ce43
Brandi Apetsi <brandiapetsi@gmail.com>
2019-09-10 21:01:00 -0400

    routes Work in progress

--- users/user-model.js
+++ users/user-model.js
@@ -4,9 +4,9 @@ module.exports = {
     find,
     findById,
     findMethods,
-    // add,
-    // update,
-    // remove
+    add,
+    update,
+    remove
 }
 
 function find() {
@@ -19,5 +19,27 @@ function findById(id) {
 }
 
 function findMethods(howto_id) {
-    return db()
+    return db('methods as m')
+        .join('howTos as h', 'h.id', 'm.howto_id')
+        .select('m.id', 'h.howTo_name', 'm.description')
+        .where({howto_id})
+}
+
+function add(howTo) {
+    const [id] = db('howTos')
+        .insert(howTo)
+    
+    return findById(id)
+}
+
+function update(id) {
+    return db('howTos')
+        .where({id})
+        .update(id)
+}
+
+function remove(id) {
+    return db('howTos')
+        .where({id})
+        .del()
 }
