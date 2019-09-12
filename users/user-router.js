98684673fa90631473e32f9274728ceec63933a6
Brandi Apetsi <brandiapetsi@gmail.com>
2019-09-11 20:50:14 -0400

    Added the rest of the requests

--- users/user-router.js
+++ users/user-router.js
@@ -60,4 +60,52 @@ router.get('/:id/methods', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+  const howtoData = req.body
+
+  try {
+    
+    const newHowTo = await HowTos.add(howtoData)  
+   
+    res.status(201).json(newHowTo) //newline
+  } catch(err) {
+    res.status(500).json({message: 'Failed to create new Hot To'})
+  }
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  
+  const count = HowTos.update(id) 
+
+  .then(count => {
+    if (count) {
+      res.json({ update: count });
+    } else {
+      res.status(404).json({ message: 'Could not find How To with that id' });
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to update this How To' });
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const count = HowTo.remove(id)
+  .then(count => {
+    if (count) {
+      res.json({ removed: count });
+    } else {
+      res.status(404).json({ message: 'Could not find How To with that id' });
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to delete this How To' });
+  });
+});
+
 module.exports = router
